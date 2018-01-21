952903e54b699e9cb03d4d56c28f4a792801d9f8
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-01-21 17:05:50 -0500

    nunjuck redner

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 /* eslint-disable */
 
 const express = require('express');
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
+
+var locals = {
+  title: 'An Example',
+  people: [
+    { name: 'Gandalf' },
+    { name: 'Frodo' },
+    { name: 'Hermoine'}
+  ]
+}
+
+const people = [
+  {name: 'Full'},
+  {name: 'Stacker'},
+  {name: 'Son'}
+];
+
+
+nunjucks.render('index.html', locals, (err, output) => {
+  console.log(output)
+});
+
+// app.use((req, res, next) => {
+//   console.log(req.method, req.path, res.statusCode)
+//   next()
+// })
+
+// app.use(/^[/][a-z]+/, (req, res, next) => {
+//   console.log('You reached the sub page')
+// })
+
+app.use(volleyball)
+
 app.get('/', (req, res) => {
-  res.send('Welcome!')
+  res.render('index', { title: 'Hall of Fame', people: people });
+  // res.send('Welcome!')
 })
 
-app.listen(3000)
+app.get('/news', (req, res) => {
+  res.send('This is the news page')
+})
+
+app.listen(3000);
